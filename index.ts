188867f13145db87a93efe172c3fe5b15f8922ea
Nathan Levi <nathan.levi@epiuse.com>
2024-03-12 22:14:41 +0200

    Busy testing

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export enum UnitType {
+enum UnitType {
     area = 'area',
     branch = 'branch',
     brand = 'brand',
@@ -58,6 +58,10 @@ class FlatUnit {
         this.type = type;
         this.tags = tags;
     }
+
+    toString(): string {
+        return JSON.stringify(this, null, 3);
+    }
 }
 
 abstract class Command<State> {
@@ -66,22 +70,31 @@ abstract class Command<State> {
     abstract redo(state: State[]): State[];
 }
 
-class CommandStack<State> {
+interface Stringifiable {
+    toString(): string;
+}
+
+class CommandStack<State extends Stringifiable> {
     private stack: Command<State>[] = [];
     private undoneStack: Command<State>[] = [];
     private _state: State[] = [];
+    private _prevState: State[] = []; // New variable to store previous state
 
     constructor(commands: Command<State>[]) {
         this.execute(commands);
     }
 
-    get state(): State[] | undefined {
+    get state(): State[] {
         return this._state;
     }
 
     execute(commands: Command<State>[]): void {
         commands.forEach(command => {
+            console.log(`Executing command: ${command.constructor.name}`);
+            console.log('State before execution:', this._state);
+            this._prevState = this._state.slice(); // Store the previous state
             this._state = command.execute(this._state);
+            console.log('State after execution:', this._state);
             this.stack.push(command);
         });
     }
@@ -89,25 +102,35 @@ class CommandStack<State> {
     undo(): void {
         const command = this.stack.pop();
         if (command) {
-            const undoneState = command.undo(this._state);
-            if (undoneState !== undefined) {
-                this.undoneStack.push(command);
-                this._state = undoneState;
-            }
+            console.log(`Undoing command: ${command.constructor.name}`);
+            console.log('State before undo:', this._state);
+            this._state = this._prevState.slice(); // Restore the previous state
+            console.log('State after undo:', this._state);
+            this.undoneStack.push(command);
         }
     }
 
     redo(): void {
         const command = this.undoneStack.pop();
         if (command) {
-            const redoneState = command.redo(this._state);
-            if (redoneState !== undefined) {
-                this.stack.push(command);
-                this._state = redoneState;
-            }
+            console.log(`Redoing command: ${command.constructor.name}`);
+            console.log('State before redo:', this._state);
+            this._state = command.redo(this._state);
+            console.log('State after redo:', this._state);
+            this.stack.push(command);
         }
     }
+
+    toString(): string {
+        let result = '';
+        this.stack.forEach((command, index) => {
+            result += `Command ${index + 1} (${command.constructor.name}):\n`;
+            result += command.toString() + '\n';
+        });
+        return result;
+    }
 }
+
 
 class Load extends Command<FlatUnit> {
     constructor(private value: FlatUnit[]) {
@@ -120,16 +143,20 @@ class Load extends Command<FlatUnit> {
     }
 
     undo(state: FlatUnit[]): FlatUnit[] {
-        throw Error('Cannot undo load');
+        throw new Error('Cannot undo Load');
     }
 
     redo(state: FlatUnit[]): FlatUnit[] {
-        throw Error('Cannot redo load');
+        throw new Error('Cannot redo Load');
+    }
+
+    toString(): string {
+        return JSON.stringify(this.value, ['id', 'code', 'name', 'type'], 3);
     }
 }
 
 class Create extends Command<FlatUnit> {
-    private originalValue?: FlatUnit[];
+    private originalValue: FlatUnit[] = [];
 
     constructor(private value: FlatUnit[]) {
         super();
@@ -137,46 +164,137 @@ class Create extends Command<FlatUnit> {
 
     execute(state: FlatUnit[]): FlatUnit[] {
         console.log('Execute Create');
-        this.originalValue = state;
-        return this.value;
+        this.originalValue = state.slice();
+        return state.concat(this.value);
     }
 
     undo(): FlatUnit[] {
         console.log('Undo Create');
-        return this.originalValue!;
+        return this.originalValue;
     }
 
     redo(): FlatUnit[] {
         console.log('Redo Create');
-        return this.execute(this.originalValue!);
+        return this.execute(this.originalValue);
+    }
+
+    toString(): string {
+        return JSON.stringify(this.value, ['id', 'code', 'name', 'type'], 3);
     }
 }
+
+class Update extends Command<FlatUnit> {
+    private originalState: FlatUnit[] = [];
+
+    constructor(private updates: FlatUnit[]) {
+        super();
+    }
+
+    execute(state: FlatUnit[]): FlatUnit[] {
+        console.log('Execute Update');
+        // Save a deep copy of the original state
+        this.originalState = JSON.parse(JSON.stringify(state));
+        this.updates.forEach(update => {
+            const index = state.findIndex(unit => unit.id === update.id);
+            if (index !== -1) {
+                state[index] = update;
+            } else {
+                throw new Error(`Cannot update unit that does not exist.`);
+            }
+        });
+        return state;
+    }
+
+    undo(): FlatUnit[] {
+        console.log('Undo Update');
+        const newState = this.originalState.map(originalUnit => {
+            const updatedUnit = this.updates.find(update => update.id === originalUnit.id);
+            return updatedUnit ? updatedUnit : originalUnit;
+        });
+        return newState;
+    }
+    redo(): FlatUnit[] {
+        console.log('Redo Update');
+        return this.execute(JSON.parse(JSON.stringify(this.originalState)));
+    }
+
+    toString(): string {
+        return JSON.stringify(this.updates, ['id', 'code', 'name', 'type'], 3);
+    }
+}
+
+class Delete extends Command<FlatUnit> {
+    private deletedUnits: FlatUnit[] = [];
+
+    constructor(private value: FlatUnit[]) {
+        super();
+    }
+
+    execute(state: FlatUnit[]): FlatUnit[] {
+        console.log('Execute Delete');
+        const unitsNotFound = this.value.filter(unit => !state.some(existingUnit => existingUnit.id === unit.id));
+        if (unitsNotFound.length > 0) {
+            throw new Error(`Cannot delete unit that does not exist.`);
+        }
+        this.deletedUnits = state.filter(unit => this.value.some(unitToDelete => unitToDelete.id === unit.id));
+        return state.filter(unit => !this.deletedUnits.some(deletedUnit => deletedUnit.id === unit.id));
+    }
+
+    undo(state: FlatUnit[]): FlatUnit[] {
+        console.log('Undo Delete');
+        return state.concat(this.deletedUnits);
+    }
+
+    redo(state: FlatUnit[]): FlatUnit[] {
+        console.log('Redo Delete');
+        return this.execute(state);
+    }
+
+    toString(): string {
+        return JSON.stringify(this.value, ['id', 'code', 'name', 'type'], 3);
+    }
+}
+
 
 const cs = new CommandStack<FlatUnit>(
     [
         new Load([
             new FlatUnit({ id: 'id01', chartId: 'chart01', clientId: 'client01', code: 'rt-01', name: 'Root', pid: '', tags: [UnitType['root']], type: UnitType['root'] }),
             new FlatUnit({ id: 'id02', chartId: 'chart01', clientId: 'client01', code: 'sr-01', name: 'Sub Root', pid: 'id01', tags: [UnitType['subRoot']], type: UnitType['subRoot'] })
-        ]),
-        new Create([
-            new FlatUnit({ id: 'id03', chartId: 'chart01', clientId: 'client01', code: 'area-01', name: 'Area', pid: 'id02', tags: [UnitType['area']], type: UnitType['area'] }),
-        ]),
-        new Create([
-            new FlatUnit({ id: 'id4', chartId: 'chart01', clientId: 'client01', code: 'area-02', name: 'Area', pid: 'id02', tags: [UnitType['area']], type: UnitType['area'] }),
-        ]),
+        ])
     ]
 );
 
-console.log('~~~~~~~~~~~~~~~~~CURRENT STATE~~~~~~~~~~~~~~~~~\n');
-console.log(cs.state); // Output the current state
-console.log('~~~~~~~~~~~~~~~~~CURRENT STATE~~~~~~~~~~~~~~~~~\n');
+cs.execute([
+    new Create([
+        new FlatUnit({ id: 'id03', chartId: 'chart01', clientId: 'client01', code: 'area-01', name: 'Area 1', pid: 'id02', tags: [UnitType['area']], type: UnitType['area'] })
+    ])
+]);
+
+cs.execute([
+    new Create([
+        new FlatUnit({ id: 'id04', chartId: 'chart01', clientId: 'client01', code: 'area-03', name: 'Area 3', pid: 'id02', tags: [UnitType['area']], type: UnitType['area'] })
+    ])
+]);
+
+cs.execute([
+    new Update([
+        new FlatUnit({ id: 'id03', chartId: 'chart01', clientId: 'client01', code: 'area-01', name: 'Area 00', pid: 'id02', tags: [UnitType['area']], type: UnitType['area'] }),
+        new FlatUnit({ id: 'id04', chartId: 'chart01', clientId: 'client01', code: 'area-02', name: 'Area 2', pid: 'id02', tags: [UnitType['area']], type: UnitType['area'] }),
+    ]),
+]);
 
 cs.undo();
-console.log('\n~~~~~~~~~~~~~~~~~STATE AFTER UNDO~~~~~~~~~~~~~~~~~\n');
-console.log(cs.state); // Output the state after undo
-console.log('\n~~~~~~~~~~~~~~~~~STATE AFTER UNDO~~~~~~~~~~~~~~~~~\n');
-
 cs.redo();
-console.log('\n~~~~~~~~~~~~~~~~~STATE AFTER REDO~~~~~~~~~~~~~~~~~\n');
-console.log(cs.state); // Output the state after redo
-console.log('\n~~~~~~~~~~~~~~~~~STATE AFTER REDO~~~~~~~~~~~~~~~~~\n');
+
+cs.execute([
+    new Delete([
+        new FlatUnit({ id: 'id04', chartId: 'chart01', clientId: 'client01', code: 'area-03', name: 'Area 3', pid: 'id02', tags: [UnitType['area']], type: UnitType['area'] })
+    ])
+]);
+
+// cs.undo();
+
+console.log(cs.toString());
+
+console.log('Final State:\n', cs.state)
